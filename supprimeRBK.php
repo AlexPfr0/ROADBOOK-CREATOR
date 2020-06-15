9f347eee8f448adff8dbb6e42c53bc9a5dccb616
alex <alex@alex-Aspire-V3-771>
2020-06-15 18:12:57 +0200

    RBK test

--- supprimeRBK.php
+++ supprimeRBK.php
@@ -1,6 +1,6 @@
 <?php
 //Supprimer le fichier .rbk uploadé pour le chargement d'un projet
 //Est appelé par la fonction supprime() de la classe RBK (RBK.js)
-$fichierRBK = '.upload/' . filter_input(INPUT_GET, 'fichierRBK');
+$fichierRBK = 'upload/' . filter_input(INPUT_GET, 'fichierRBK');
 
 unlink($fichierRBK);
